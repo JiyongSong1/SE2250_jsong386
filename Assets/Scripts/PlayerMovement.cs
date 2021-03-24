8f650accd4dc1e5556eb956c25fac021e1dc7c2e
AndrewLou2 <71093342+AndrewLou2@users.noreply.github.com>
2021-03-24 02:01:44 -0400

    Update PlayerMovement.cs
    The player can dash left and right when "A" or "D" key is double tapped within a certain time frame. Use this code and implement into the PlayercController.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,8 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
+    public float dashDistance = 15f;
+    bool isDashing;
+    float doubleTapTime;
+
+    //private Rigidbody2D myRigidbody; in player controller
+    private Rigidbody2D rb;
+    KeyCode lastKeyCode;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,35 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Dashing left
+        if (Input.GetKeyDown(lastKeyCode.A){
+            if (doubleTapTime > Time.time && lastKeyCode == KeyCode.A){
+                //dash
+                StartCoroutine(Dash(-1f));
+            } else {
+                doubleTapTime = Time.time + 0.3f;
+            }
+
+            lastKeyCode keyCode.A;
+        }
+        //Dashing right
+        if (Input.GetKeyDown(lastKeyCode.D){
+            if (doubleTapTime > Time.time && lastKeyCode == KeyCode.D){
+                //dash
+                StartCoroutine(Dash(1f));
+            } else {
+                doubleTapTime = Time.time + 0.3f;
+            }
+
+            lastKeyCode keyCode.D;
+        }
     }
+    IEnumerator Dash (float direction) {
+        isDashing = true;
+        rb.velocity = new Vector2(rb.velocity.x, 0f);
+        rb.AddForce(new Vector2(dashDistance * direction, 0f), ForceMode2D.Impulse);
+        yield return new WaitForSeconds(0.4f);
+        isDashing = false;
+    }
+    
 }
