ef231bed8390de616cfeeb7d229c8cd5d7842ff3
JiyongSong1 <jsong386@uwo.ca>
2021-04-13 19:42:53 -0400

    Edited arrow script
    The one shot issue with damage player script came back, so i attached arrow script to sword instead. Also deleted the destroying arrow since it is done automatically after 4 seconds in player controller script

--- Assets/__Scripts/PlayerController.cs
+++ Assets/__Scripts/PlayerController.cs
@@ -67,8 +67,8 @@ public class PlayerController : Arrow
             {
             GameObject arrow = Instantiate(arrowPrefab, transform.position, Quaternion.Euler(0, 0, 90));
             arrow.GetComponent<Rigidbody2D>().velocity = new Vector2 (Input.GetAxisRaw("Horizontal")*7f ,0f);
-            //destroy arrow after 7 seconds automatically
-            Destroy (arrow, 7.0f);
+            //destroy arrow after 4 seconds automatically
+            Destroy (arrow, 4.0f);
             }
         }
         if (Input.GetAxisRaw("Vertical") > 0.5f ||Input.GetAxisRaw("Vertical") < -0.5f)
