6158f56f7eadac142265582040fdd4f181b91c81
JiyongSong1 <jsong386@uwo.ca>
2021-04-12 20:09:12 -0400

    New enemy
    Added a ninja that shoots fireball, but no animations on it

---
+++ Assets/__Scripts/FireBall.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBall : MonoBehaviour
+{
+    float moveSpeed = 3f;
+    Rigidbody2D rigidbody;
+
+    public GameObject target;
+    Vector2 moveDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        target = GameObject.Find("Player");
+        moveDirection = (target.transform.position - transform.position).normalized * moveSpeed;
+        rigidbody.velocity = new Vector2(moveDirection.x, moveDirection.y);
+        Destroy(gameObject, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name.Equals("Player"))
+        {
+            Destroy (gameObject);
+        }
+    }
+}
---
+++ Assets/__Scripts/NinjaAttack.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NinjaAttack : MonoBehaviour
+{
+    [SerializeField]
+    GameObject fireBall;
+
+    float fireRate;
+    float nextFire;
+    void Start()
+    {
+        fireRate = 3f;
+        nextFire = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckTimeForFire();
+    }
+    
+    void CheckTimeForFire()
+    {
+        if (Time.time > nextFire){
+            Instantiate (fireBall, transform.position, Quaternion.identity);
+            nextFire = Time.time + fireRate;
+        }
+    }
+}
