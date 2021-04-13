e16d1ab9a423e9ddc5215c6aede91d24b91823aa
AndrewLou2 <71093342+AndrewLou2@users.noreply.github.com>
2021-04-12 21:39:54 -0400

    dd
    fixed animations

--- Assets/__Scripts/Arrow.cs
+++ Assets/__Scripts/Arrow.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,7 +9,7 @@ public class Arrow : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //changed animations
     }
 
     void OnTriggerEnter2D(Collider2D col)
