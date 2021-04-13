8cb85d9341eb2ef4f333e3caafd925fd57bc7342
hpark <71079174+hpark349@users.noreply.github.com>
2021-04-13 00:40:38 -0400

    map
    add maps folder

---
+++ Assets/_maps/base tileset.tsx
@@ -0,0 +1,187 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="base tileset" tilewidth="16" tileheight="16" spacing="1" tilecount="1767" columns="57">
+ <image source="roguelike-pack/Spritesheet/roguelikeSheet_transparent.png" width="968" height="526"/>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.889772" y="1.17577" width="15.0626" height="14.7448"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0953327" y="2.73287" width="15.7617" height="12.9017"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0635551" y="1.04866" width="14.9672" height="14.8719"/>
+  </objectgroup>
+ </tile>
+ <tile id="59">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2.09732" y="0.0635551" width="13.855" height="15.8252"/>
+  </objectgroup>
+ </tile>
+ <tile id="60">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0317776" y="0" width="15.8888" height="15.8888"/>
+  </objectgroup>
+ </tile>
+ <tile id="61">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0317776" y="0" width="14.0457" height="15.9206"/>
+  </objectgroup>
+ </tile>
+ <tile id="116">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.953327" y="0.0635551" width="14.999" height="14.9355"/>
+  </objectgroup>
+ </tile>
+ <tile id="117">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0635551" y="0.0635551" width="15.857" height="14.8083"/>
+  </objectgroup>
+ </tile>
+ <tile id="118">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0.0317776" width="14.3635" height="14.3317"/>
+  </objectgroup>
+ </tile>
+ <tile id="529">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.635551" y="0.826216" width="14.2681" height="15.2532"/>
+  </objectgroup>
+ </tile>
+ <tile id="640">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.130435" y="0" width="15.7391" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="641">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.217391" y="0.130435" width="15.5652" height="15.7391"/>
+  </objectgroup>
+ </tile>
+ <tile id="642">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0434783" y="0.130435" width="15.7826" height="15.6957"/>
+  </objectgroup>
+ </tile>
+ <tile id="643">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0635551" y="0.0635551" width="15.8888" height="15.9206"/>
+  </objectgroup>
+ </tile>
+ <tile id="644">
+  <objectgroup draworder="index" id="7">
+   <object id="8" x="0.0434783" y="0.130435" width="16.087" height="15.8696"/>
+  </objectgroup>
+ </tile>
+ <tile id="645">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0869565" y="0.173913" width="15.8261" height="15.8261"/>
+  </objectgroup>
+ </tile>
+ <tile id="650">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.173913" y="0.217391" width="15.7391" height="15.5652"/>
+  </objectgroup>
+ </tile>
+ <tile id="1214">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="0" y="0" width="16" height="16"/>
+   <object id="3" x="0.0434783" y="-0.0434783" width="15.8696" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1215">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0434783" y="0" width="15.8696" height="15.913"/>
+  </objectgroup>
+ </tile>
+ <tile id="1216">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0434783" y="0" width="16" height="15.9565"/>
+  </objectgroup>
+ </tile>
+ <tile id="1217">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="15.1304" y="0.0434783">
+    <polygon points="0,0 -14.9565,15.6087 0.782609,15.8261 0.782609,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1218">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0869565" y="0.0434783">
+    <polygon points="0,0 15.7826,15.8696 0.0869565,15.8261"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1271">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0434783" y="-0.0434783" width="15.913" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1272">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0434783" y="0.0434783" width="16" height="15.9565"/>
+  </objectgroup>
+ </tile>
+ <tile id="1273">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0434783" y="0.0434783" width="15.9565" height="15.913"/>
+  </objectgroup>
+ </tile>
+ <tile id="1274">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="15.9565" height="15.913"/>
+  </objectgroup>
+ </tile>
+ <tile id="1275">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0434783" y="0.0434783" width="15.913" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1328">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16.0435" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1329">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0434783" y="0" width="15.913" height="15.8696"/>
+  </objectgroup>
+ </tile>
+ <tile id="1330">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0434783" y="0.0434783" width="15.9565" height="15.8696"/>
+  </objectgroup>
+ </tile>
+ <tile id="1331">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="15.7826" y="2.6087">
+    <polygon points="0,0 -12.9565,13.1304 -15.8696,13.3043 -15.7391,-2.65217 -0.0434783,-2.47826"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1332">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.347826" y="2.04348">
+    <polygon points="0,0 14.0435,13.7826 15.6087,13.6957 15.4348,-1.95652 -0.130435,-1.86957"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <wangsets>
+  <wangset name="Dirt" type="corner" tile="578">
+   <wangcolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="520" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="521" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="522" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="577" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="578" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="579" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="634" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="635" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="636" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
